acfb8e4462de089d4b20b5e99cf204777d99d56f
leandrodklein <leklein@gmail.com>
2022-02-22 20:21:06 -0300

    first version

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,24 @@
+import { Frase } from '../shared/frase.model'
+
+export const FRASES: Frase[] = [
+    {
+        fraseEng: 'I like to learn',
+        frasePtBr: 'Eu gosto de aprender'
+    },
+
+    {
+        fraseEng: 'I watch tv',
+        frasePtBr: 'Eu assisto tv'
+    },
+
+    {
+        fraseEng: 'I am hungry',
+        frasePtBr: 'Eu estou com fome'
+    },
+
+    {
+        fraseEng: 'How are you ?',
+        frasePtBr: 'Como vai voce?'
+    }
+
+]
